3b1fb29765bde926b585078dd68672323b9105c2
Christian Callahan <callahan.0019@gmail.com>
2025-12-14 00:12:59 -0500

    first db migration

---
+++ apps/cms/src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20251214_051038 from './20251214_051038';
+
+export const migrations = [
+  {
+    up: migration_20251214_051038.up,
+    down: migration_20251214_051038.down,
+    name: '20251214_051038'
+  },
+];
